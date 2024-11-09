50c9a8da8fbfc5520b868b2f2f48239741c348f1
Amina Rakhbari <amina.rakhbari@yandex.ru>
2024-11-09 14:11:01 +0300

    Добавила создание и удаление карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
-// @todo: Темплейт карточки
+// темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// DOM узел списка карточек
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// функция создания карточки
+function createCard(cardData, deleteCard) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardTitle = cardElement.querySelector('.card__title');
+  const cardImage = cardElement.querySelector('.card__image');
 
-// @todo: Функция удаления карточки
+  cardTitle.textContent = cardData.name;
+  cardImage.src = cardData.link;
 
-// @todo: Вывести карточки на страницу
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+  cardDeleteButton.addEventListener('click', function(evt) {
+    const cardToDelete = evt.target.closest('.card');
+    deleteCard(cardToDelete);
+  });
+
+  return cardElement;
+}
+
+// функция удаления карточки
+function deleteCard(cardToDelete) {
+  cardToDelete.remove();
+}
+
+// вывод всех карточек на страницу
+initialCards.forEach((cardData) => {
+  const cardElement = createCard(cardData, deleteCard);
+  placesList.append(cardElement);
+});
